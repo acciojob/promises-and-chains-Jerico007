f12655eb9ffffcde0fce815813641400e7a93f19
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-08-04 14:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let element = document.getElementById("form");
+
+let obj = {};
+element.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+
+  let prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000);
+  });
+
+  prom.then((data)=>{
+    alert(`Welcome, ${data}. You can vote.`);
+  })
+
+  prom.catch((data)=>{
+    alert(`Oh sorry ${data}. You aren't old enough.`);
+  })
+});
